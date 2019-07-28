7514abee57272c651c8f9d8de1e846cb772ff70c
Sahil <sahilgoel755@gmail.com>
2019-07-28 16:03:40 +0530

    E-content page done

--- excelUpload.php
+++ excelUpload.php
@@ -1,23 +1,35 @@
 <?php
 set_time_limit(0);
 
+if (!isset($_GET['file'])) {
+  header('Location: /');
+}
+
+$file  = './econtent/' . $_GET['file'];
+
+// check if the file exists?
+if (!file_exists($file)) {
+  header('Location: /');
+}
+
 include 'header.html';
 
 
 // getting temporary source of excel file
-include("PHPExcel-1.8/Classes/PHPExcel/IOFactory.php"); // Add PHPExcel Library in this code
+include("PHPExcel-1.8/Classes/PHPExcel/IOFactory.php"); // Add PHPExcel Library in this code\
 
-$objPHPExcel = PHPExcel_IOFactory::load('uploads/upload.xlsx'); // create object of PHPExcel library by using load() method and in load method define path of selected file
+$objPHPExcel = PHPExcel_IOFactory::load($file); // create object of PHPExcel library by using load() method and in load method define path of selected file
 
 echo "<h1 class='mt-3'><center>Econtent</center></h1>";
 
-echo '<table class="table table-bordered table-condensed table-responsive table-striped">
+echo '<div class="container-fluid">
+<table class="table table-bordered table-condensed table-striped">
   <thead>
-    <tr width="100%">
-      <th scope="col" width="25%">Topic</th>
-      <th scope="col" width="25%">Book Reference</th>
-      <th scope="col" width="25%">Web Reference</th>
-      <th scope="col" width="25%">Video Reference</th>
+    <tr style="width:100%">
+      <th scope="col" style="width:40%">Topic</th>
+      <th scope="col" style="width:20%">Book Reference</th>
+      <th scope="col" style="width:20%">Web Reference</th>
+      <th scope="col" style="width:20%">Video Reference</th>
     </tr>
   </thead>
   <tbody>';
@@ -26,32 +38,31 @@ foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
   $highestRow = $worksheet->getHighestRow();
 
   for ($row = 2; $row <= $highestRow; $row++) {
-
     $topic = $worksheet->getCellByColumnAndRow(0, $row)->getValue();
     $book = $worksheet->getCellByColumnAndRow(1, $row)->getValue();
     $link = $worksheet->getCellByColumnAndRow(2, $row)->getValue();
     $video = $worksheet->getCellByColumnAndRow(3, $row)->getValue();
 
     if ($topic) {
-      echo "<tr width='100%'>
-      <td width='25%'>" . htmlspecialchars($topic) . "</td>";
+      echo "<tr style='width:100%'>
+      <td style='width:40%'>" . htmlspecialchars($topic) . "</td>";
 
       if ($book) {
-        echo "<td width='25%'><a href='$book'>Open PDF</a></td>";
+        echo "<td style='width:20%'><a href='$book'>Open PDF</a></td>";
       } else {
-        echo "<td width='25%'></td>";
+        echo "<td style='width:20%'>--</td>";
       }
 
       if ($link) {
-        echo "<td width='25%'><a href='$link'>Read</a></td>";
+        echo "<td style='width:20%'><a href='$link'>Read</a></td>";
       } else {
-        echo "<td width='25%'></td>";
+        echo "<td style='width:20%'>--</td>";
       }
 
       if ($video) {
-        echo "<td width='25%'><a href='$video'>Watch</a></td>";
+        echo "<td width='20%'><a href='$video'>Watch</a></td>";
       } else {
-        echo "<td width='25%'></td>";
+        echo "<td width='20%'>--</td>";
       }
 
       echo "</tr>";
@@ -60,6 +71,7 @@ foreach ($objPHPExcel->getWorksheetIterator() as $worksheet) {
 }
 
 echo "</tbody>
-</table>";
+</table>
+</div>";
 
 include 'footer.html';
