ab900faca57eb4e3579feb475100b004670956f8
vanshkamboj <vanshkamboj.mca@gmail.com>
2019-07-29 19:06:53 -0700

    add new files

---
+++ paper_mcasem1.php
@@ -0,0 +1,21 @@
+<?php
+include("header.html");
+?>
+<div class="container my-5">
+<div class="list-group">
+  <button type="button" class="list-group-item  text-center list-group-item-action active">
+    Semester I
+  </button>
+  <button type="button" onclick="window.location.href = 'University Question Papers/BCA/BCA 6th Semester/BCA-361/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>MCA-14-11</b>PROGRAMMING IN C  </button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BCA/BCA 6th Semester/BCA-362/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>>MCA-14-12</b>COMPUTER ORGANIZATION  </button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BCA/BCA 6th Semester/BCA-363/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>>MCA-14-13</b>SOFTWARE ENGINEERING  </button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BCA/BCA 6th Semester/BCA-364/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>>MCA-14-14</b>DISCRETE MATHEMATICAL STRUCTURES  </button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BCA/BCA 6th Semester/BCA-365/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>MCA-14-15</b>COMPUTER ORIENTED NUMERICAL AND STATISTICAL METHODS  </button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BCA/BCA 6th Semester/BCA-366/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>MCA-14-16 </b>S/W LAB – I BASED ON MCA-14-11  </button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BCA/BCA 6th Semester/BCA-366/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>MCA-14-17</b> S/W LAB – II BASED ON MCA-14-15  </button>
+</div>
+</div>
+
+<?php
+include("footer.html");
+?>
