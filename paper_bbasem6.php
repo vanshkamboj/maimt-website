c9935f65152fe13ba03fdaf3e058cc23e0b539e1
vanshkamboj <vanshkamboj.mca@gmail.com>
2019-07-29 20:58:14 -0700

    add

--- paper_bbasem6.php
+++ paper_bbasem6.php
@@ -6,12 +6,12 @@ include("header.html");
   <button type="button" class="list-group-item  text-center list-group-item-action active">
     Semester VI
   </button>
-<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-302/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-308 </b>Entrepreneurship Development</button>
-<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-302/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-309 </b>Business Laws-ii</button>
-<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-302/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-310 </b>Logistic Management</button>
-<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-302/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-311 </b>Principles of Insurance</button>
-<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-302/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-312 </b>Introduction of Financial Services</button>
-<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-302/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-313 </b>Viva-Voce</button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-308/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-308 </b>Entrepreneurship Development</button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-309/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-309 </b>Business Laws-ii</button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-310/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-310 </b>Logistic Management</button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-311/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-311 </b>Principles of Insurance</button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-312/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-312 </b>Introduction of Financial Services</button>
+<button type="button" onclick="window.location.href = 'University Question Papers/BBA/BBA 6th Semester/BBA-313/prepaper_bca.php';" class="list-group-item list-group-item-action"><b>BBA-313 </b>Viva-Voce</button>
   
 </div>
 </div>
